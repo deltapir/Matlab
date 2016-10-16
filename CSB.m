a4fc041ebcf9b20b69ffcb96bb4189fb0b0b0f4a
deltapir <deltapirandello@live.com>
2016-10-16 23:15:26 +0200

    tiny fix
    ho tolto qualche riga e mangiato un po' di pasta

--- CSB.m
+++ CSB.m
@@ -1,14 +1,16 @@
 clear all
 close all
 clc
+ma se tipo aggiungessi tt sta roba qua?
 
+credo che sia meglio mangiare
 %% INPUT
 fluid='r134a';
 Tcella=-10; %°C
-Tamb=20;
+#Tamb=20;
 Qev=0.2; %kW
 DT_sur=0;
-DT_sub=0;
+
 DT_min_ev=10;
 DT_min_covett=0:5:50;
 sono bellissimo
